20ec8bf67a1cb217e37284ba8beb76587ce4b698
Ian Hussey <ianhusseysipod@gmail.com>
2024-07-17 13:13:24 +0200

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+#' PORT: Positive-Definiteness Of (Pearson's) r Tables
+#'
+#'Check correlation tables for inconsistencies and attempt to isolate which specific correlation estimates may be causing the inconsistency. 
+#'
+#' @section Important:
+#' Package is in early stage development.
+#'
+#' @docType package
+#' @name PORT
+NULL
